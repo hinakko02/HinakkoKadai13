6fd1520c14d1c7df930f875645be2fbe9a1d9b6d
hinakko <131275914+hinakko02@users.noreply.github.com>
2023-07-12 21:08:11 +0900

    FirstCommit

---
+++ HinakkoKadai13/ItemTableViewCell.swift
@@ -0,0 +1,15 @@
+//
+//  ItemTableViewCell.swift
+//  HinakkoKadai13
+//
+//  Created by Hina on 2023/07/11.
+//
+
+import UIKit
+
+class ItemTableViewCell: UITableViewCell {
+
+    @IBOutlet weak var checkImageView: UIImageView!
+    @IBOutlet weak var nameLabel: UILabel!
+    
+}
